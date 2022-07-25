badc2dae6bed768324140cba9a0ac67d58019a2c
Ubutnu <you@example.com>
2022-07-25 11:26:49 +0530

    update

--- src/component/dashboard/dash.js
+++ src/component/dashboard/dash.js
@@ -1,24 +1,47 @@
 import './dash.css';
-import { Link } from 'react-router-dom';
 import {useDispatch} from 'react-redux'
 import { logout } from '../login/login_slice';
+import axios from 'axios';
+import { useState,useEffect } from "react";
 
 
 const Dashboard = () =>{
-    // const navigate = useNavigate()
-
-    // const nav =()=>{
-    //     Navigate("/logout")
-    // }
-
     const dispatch = useDispatch()
 
+    const [apiResponse, setApiResponse] = useState([]);
+
+    useEffect(()=>{
+        axios.get('https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline').then((response)=>{
+       
+        setApiResponse(response.data)
+        }).catch(err=>{
+            console.log(err)
+        })
+
+    },[])
     return(
         <>
         <img className="img-dash" src="./images/dash.webp" alt='dashboard'/>
         {/* <Link to="/logout"> <button className='btn-btn'>LOG OUT</button> </Link> */}
         <button className='btn-btn'onClick={() =>{ localStorage.clear() ;dispatch(logout())}}>LOG OUT</button>
+
+        {
+        apiResponse?.map((Element)=>{
+             return (
+                <>
+                    <p>{Element?.id}</p>
+                    <p>{Element?.brand}</p>
+                    <p>{Element.description}</p>
+                    <img src={Element?.image_link} alt='img' />
+                    <p>{Element?.name}</p>
+                    <p>{Element?.price}</p>
+                </> 
+            )
+        })
+    }
+
         </>
+
     )
 }
 export default Dashboard;
